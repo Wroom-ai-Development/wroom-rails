68d5f1530ae5c932929e65fd56c81cbc9c7926e9
Marcel Wojdyło <marcel.wojdylo@tdcm.io>
2023-06-17 18:16:00 +0200

    add users index and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :documents
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
 end
