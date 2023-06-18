f6975825ab985204be1f0bd2330e04872d90145b
Marcel Wojdyło <marcel.wojdylo@tdcm.io>
2023-06-18 16:18:54 +0200

    add user specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,24 +12,6 @@ class UsersController < ApplicationController
     @documents = user.documents
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'user was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def edit; end
 
   def update
