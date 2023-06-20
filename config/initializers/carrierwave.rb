c77a4984b78e5fc1c8ed3b3730ce97f05495c563
Marcel Wojdyło <marcel.wojdylo@tdcm.io>
2023-06-20 14:08:05 +0200

    adjust carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ unless Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'Google',
-      google_storage_access_key_id: '',
-      google_storage_secret_access_key: ENV['GOOGLE_CLOUD_STORAGE_API_KEY']
+      google_storage_access_key_id: ENV['GOOGLE_STORAGE_ACCESS_KEY_ID'],
+      google_storage_secret_access_key: ENV['GOOGL_STORAGE_API_KEY']
     }
     config.fog_directory = 'wroom-rails'
   end
