2e60824733ec008257467501b65abf49258365d3
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-26 18:29:18 +0200

    add redirect after chunks loaded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_155847) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_154506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_155847) do
     t.bigint "document_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "last", default: false
     t.index ["document_id"], name: "index_document_chunks_on_document_id"
   end
 
