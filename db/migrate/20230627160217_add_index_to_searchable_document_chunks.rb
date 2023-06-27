e795761d5de8eda31dd17514f3d4571dadd81618
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-27 19:53:53 +0200

    remove unnecessary migrations

--- db/migrate/20230627160217_add_index_to_searchable_document_chunks.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexToSearchableDocumentChunks < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
-  def change
-    add_index :document_chunks, :searchable, using: :gin, algorithm: :concurrently
-  end
-end
--- db/migrate/20230627174020_remove_last_from_document_chunks.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveLastFromDocumentChunks < ActiveRecord::Migration[7.0]
-  def up
-    remove_column :document_chunks, :last
-  end
-
-  def down; end
-end
