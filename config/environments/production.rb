b4212888489959ea452256f5ffe5842f95ac163e
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-28 16:50:46 +0200

    configure action mailer for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_uptions {
+    host: 'wroom-rails-043ae6af063d.herokuapp.com/'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
