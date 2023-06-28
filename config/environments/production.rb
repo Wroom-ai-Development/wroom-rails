43de0b14d38e6b13204afaf91e9832348fce2e2d
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-28 17:04:10 +0200

    remove port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     host: 'wroom-rails-043ae6af063d.herokuapp.com'
   }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'wroom-rails-043ae6af063d.herokuapp.com', port: 25 }
+  config.action_mailer.smtp_settings = { address: 'wroom-rails-043ae6af063d.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
