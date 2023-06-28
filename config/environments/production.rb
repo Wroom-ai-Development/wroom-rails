8e1c444e4a9614763654d95c658ce975e84abf4d
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-28 16:55:44 +0200

    remove slash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: 'wroom-rails-043ae6af063d.herokuapp.com/'
+    host: 'wroom-rails-043ae6af063d.herokuapp.com'
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
