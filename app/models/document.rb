e6af7bed81ed2b07c0213ab906a89b758c8a9279
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-06-30 09:39:18 +0200

    validate year published

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,12 +2,14 @@
 
 class Document < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, uniqueness: { scope: :user_id }
-  validates :file, presence: true
   has_one_attached :file
   has_many :context_references, dependent: :destroy
   has_many :conversations, through: :context_references
   has_many :document_chunks, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :file, presence: true
+  validates :year_published, numericality: { only_integer: true }, length: { in: 0..4 }, allow_nil: true
 
   def parse_document_chunks_from_file
     raw_text = if file.content_type == 'application/pdf'
