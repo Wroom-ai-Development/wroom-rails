de44abcbe38d3c139d9b166c7c9a44f698835f68
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-07-01 17:24:47 +0200

    add stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,8 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="editor"
 export default class extends Controller {
   connect() {
-    console.log("yay")
+    this.element.textContent = "Hello World!"
   }
 }
