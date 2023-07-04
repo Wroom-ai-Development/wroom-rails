3f429fee7f476cbe579821b7c72af2f1a63c8530
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-07-04 15:11:21 +0200

    create new layout

---
+++ app/controllers/wroom_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class WroomController < ApplicationController
+  layout 'wroom'
+
+  def app
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/monitoring', to: 'monitoring#index'
-  root 'documents#index'
+  get '/wroom', to: 'wroom#app'
+  root 'wroom#app'
 end
