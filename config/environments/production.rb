3bd88cf25c675c50b029d2ddf30f4a8ce3b77c6f
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-08-04 14:15:51 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.serve_static_files = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
