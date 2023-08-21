1a658d788b5520c2020c1ad5dfe2f7a417c052e3
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-08-21 18:49:29 +0200

    add mana counter

--- app/models/usage_record.rb
+++ app/models/usage_record.rb
@@ -3,4 +3,51 @@
 class UsageRecord < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
+
+  GPT_4_INPUT_PRICE_PER_1000_TOKENS = 0.03
+  GPT_4_OUTPUT_PRICE_PER_1000_TOKENS = 0.06
+  GPT_3_5_TURBO_16K_INPUT_PRICE_PER_1000_TOKENS = 0.003
+  GPT_3_5_TURBO_16K_OUTPUT_PRICE_PER_1000_TOKENS = 0.004
+  GPT_3_5_TURBO_INPUT_PRICE_PER_1000_TOKENS = 0.0015
+  GPT_3_5_TURBO_OUTPUT_PRICE_PER_1000_TOKENS = 0.002
+
+  def total_price
+    gpt_4_price + gpt_3_5_turbo_16k_price + gpt_3_5_turbo_price
+  end
+
+  def gpt_4_price
+    gpt_4_input_price + gpt_4_output_price
+  end
+
+  def gpt_4_input_price
+    gpt_4_input_tokens / 100.0 * GPT_4_INPUT_PRICE_PER_1000_TOKENS
+  end
+
+  def gpt_4_output_price
+    gpt_4_output_tokens / 100.0 * GPT_4_OUTPUT_PRICE_PER_1000_TOKENS
+  end
+
+  def gpt_3_5_turbo_16k_price
+    gpt_3_5_turbo_16k_input_price + gpt_3_5_turbo_16k_output_price
+  end
+
+  def gpt_3_5_turbo_16k_input_price
+    gpt_3_5_turbo_16k_input_tokens / 100.0 * GPT_3_5_TURBO_16K_INPUT_PRICE_PER_1000_TOKENS
+  end
+
+  def gpt_3_5_turbo_16k_output_price
+    gpt_3_5_turbo_16k_output_tokens / 100.0 * GPT_3_5_TURBO_16K_OUTPUT_PRICE_PER_1000_TOKENS
+  end
+
+  def gpt_3_5_turbo_price
+    gpt_3_5_turbo_input_price + gpt_3_5_turbo_output_price
+  end
+
+  def gpt_3_5_turbo_input_price
+    gpt_3_5_turbo_input_tokens / 100.0 * GPT_3_5_TURBO_INPUT_PRICE_PER_1000_TOKENS
+  end
+
+  def gpt_3_5_turbo_output_price
+    gpt_3_5_turbo_output_tokens / 100.0 * GPT_3_5_TURBO_OUTPUT_PRICE_PER_1000_TOKENS
+  end
 end
