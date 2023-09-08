c4916acb442a8f8ecaf2b4732522ecdaa63b37eb
Marcel Wojdylo <marcel.wojdylo@wroom.ai>
2023-09-08 15:08:02 +0200

    lint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.assets.precompile += %w( '.svg' )  
+  config.assets.precompile += %w[.svg]
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
